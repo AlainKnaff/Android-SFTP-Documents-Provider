3c86b6747905ce33ea3ee18d8930a76ff9fdae73
Alain Knaff <alain@knaff.lu>
2025-12-20 08:57:48 +0100

    Remove unneeded imports

--- app/src/main/java/com/island/androidsftpdocumentsprovider/MainActivity.kt
+++ app/src/main/java/com/island/androidsftpdocumentsprovider/MainActivity.kt
@@ -1,7 +1,5 @@
 package com.island.androidsftpdocumentsprovider
 
-import java.util.List
-
 import android.net.Uri;
 import android.app.Activity
 import android.app.AlertDialog
@@ -10,7 +8,6 @@ import android.content.Intent
 import android.content.ComponentName
 import android.os.Build
 import android.os.Bundle
-import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
