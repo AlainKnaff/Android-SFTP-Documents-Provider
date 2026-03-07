12da6acc8bdc324b74a5fa76fc239e5a86225f95
Alain Knaff <alain@knaff.lu>
2026-03-07 21:36:49 +0100

    Allow to launch multiple file browsers

--- app/src/main/java/com/island/androidsftpdocumentsprovider/MainActivity.kt
+++ app/src/main/java/com/island/androidsftpdocumentsprovider/MainActivity.kt
@@ -74,6 +74,8 @@ class MainActivity : ProviderActivity()
 						  "com.android.documentsui.files.FilesActivity"))
 		intent.setData(("content://"+getAuthority()+
 				    "/root").toUri())
+		intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK or
+				Intent.FLAG_ACTIVITY_MULTIPLE_TASK);
 		startActivity(intent)
 		return
 	    } catch(e: Exception) {
